c89eecc2d70edb68e97267e65186b4fa62f76732
Oleh98Hordon <oleg98gordon@gmail.com>
2021-05-02 16:22:09 +0000

    02.05.21

---
+++ apply.sh
@@ -0,0 +1,8 @@
+kubectl apply -f pv-nginx.yaml \
+-f php-service.yaml \
+-f nginx-configmap.yaml \
+-f nginx-deploy.yaml \
+-f secret.yaml \
+-f php-fpm-deploy.yaml \
+-f mysql-deployment.yaml \
+-f phpmyadmin.yaml
